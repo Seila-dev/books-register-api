d454551c89521d889890da1c6ef68ea795edad2b
Seila-Dev <erickrodrigues3975@gmail.com>
2025-01-14 16:41:42 -0300

    cors tentativa 7

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // app.use(cors())
 
 app.use(cors({
-    origin: 'http://localhost:5173/',
+    origin: '*',
     methods: ['GET', 'POST', 'DELETE', 'PUT'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, 
