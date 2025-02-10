29cf7fc13bbd931ef5479771009568ac1acfa3b8
Seila-Dev <erickrodrigues3975@gmail.com>
2025-02-10 01:59:51 -0300

    fix: #6 fix products and cat. types

--- src/routes/categories-routes.ts
+++ src/routes/categories-routes.ts
@@ -6,6 +6,6 @@ const categoriesRoutes = Router()
 categoriesRoutes.post("/", new CategoriesController().create)
 categoriesRoutes.get("/", new CategoriesController().findAll)
 categoriesRoutes.delete("/:id", new CategoriesController().delete)
-categoriesRoutes.put("/:id", new CategoriesController().update);
+categoriesRoutes.put("/:id", new CategoriesController().update)
 
 export default categoriesRoutes
