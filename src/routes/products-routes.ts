6ba07159b467e70e77cb65e6f8fc54dd3aeb3aea
Seila-Dev <erickrodrigues3975@gmail.com>
2025-02-11 01:31:28 -0300

    fix: not added product route

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -22,7 +22,7 @@ productsRoutes.delete("/:id", new ProductsController().delete)
 productsRoutes.put("/:id", upload.single('file'), (req, res) => {
     new ProductsController().update(req, res);
 })
-// productsRoutes.get("/:id/categories", new ProductsController().findCategories)
-// productsRoutes.post("/:id/categories", new ProductsController().addCategory)
+productsRoutes.get("/:id/categories", new ProductsController().findCategories)
+productsRoutes.post("/:id/categories", new ProductsController().addCategory)
 
 export default productsRoutes;
